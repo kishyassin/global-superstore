e2233a660403316376fa7483d03138be38bf4253
KICH Yassine <kishyassin@gmail.com>
2024-08-28 14:09:07 +0100

    dependency injection

--- src/main/java/com/ltp/globalsuperstore/repository/StoreRepository.java
+++ src/main/java/com/ltp/globalsuperstore/repository/StoreRepository.java
@@ -3,8 +3,11 @@ package com.ltp.globalsuperstore.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.ltp.globalsuperstore.Item;
 
+@Repository
 public class StoreRepository {
     private List<Item> items = new ArrayList<>();
 
